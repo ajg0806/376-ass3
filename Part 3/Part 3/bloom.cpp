d43ec159d36ab88a386eee71dc518850a4ffab93
ajg246 <grabsch.adam@gmail.com>
2018-10-13 17:30:13 +1100

    SAVE ME!!

--- Part 3/Part 3/bloom.cpp
+++ Part 3/Part 3/bloom.cpp
@@ -52,53 +52,53 @@ cl_device_id create_device() {
 /* Create program from a file and compile it */
 cl_program build_program(cl_context ctx, cl_device_id dev, const char* filename) {
 
-   cl_program program;
-   FILE *program_handle;
-   char *program_buffer, *program_log;
-   size_t program_size, log_size;
-   int err;
+	cl_program program;
+	FILE *program_handle;
+	char *program_buffer, *program_log;
+	size_t program_size, log_size;
+	int err;
 
-   /* Read program file and place content into buffer */
-   program_handle = fopen(filename, "rb");
-   if(program_handle == NULL) {
-      perror("Couldn't find the program file");
-      exit(1);
-   }
-   fseek(program_handle, 0, SEEK_END);
-   program_size = ftell(program_handle);
-   rewind(program_handle);
-   program_buffer = (char*)malloc(program_size + 1);
-   program_buffer[program_size] = '\0';
-   fread(program_buffer, sizeof(char), program_size, program_handle);
-   fclose(program_handle);
+	/* Read program file and place content into buffer */
+	program_handle = fopen(filename, "rb");
+	if (program_handle == NULL) {
+		perror("Couldn't find the program file");
+		exit(1);
+	}
+	fseek(program_handle, 0, SEEK_END);
+	program_size = ftell(program_handle);
+	rewind(program_handle);
+	program_buffer = (char*)malloc(program_size + 1);
+	program_buffer[program_size] = '\0';
+	fread(program_buffer, sizeof(char), program_size, program_handle);
+	fclose(program_handle);
 
-   /* Create program from file */
-   program = clCreateProgramWithSource(ctx, 1, 
-      (const char**)&program_buffer, &program_size, &err);
-   if(err < 0) {
-      perror("Couldn't create the program");
-      exit(1);
-   }
-   free(program_buffer);
+	/* Create program from file */
+	program = clCreateProgramWithSource(ctx, 1,
+		(const char**)&program_buffer, &program_size, &err);
+	if (err < 0) {
+		perror("Couldn't create the program");
+		exit(1);
+	}
+	free(program_buffer);
 
-   /* Build program */
-   err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
-   if(err < 0) {
+	/* Build program */
+	err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+	if (err < 0) {
 
-      /* Find size of log and print to std output */
-      clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG, 
-            0, NULL, &log_size);
-      program_log = (char*) malloc(log_size + 1);
-      program_log[log_size] = '\0';
-      clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG, 
-            log_size + 1, program_log, NULL);
-      printf("%s\n", program_log);
-      free(program_log);
-	  getchar();
-      exit(1);
-   }
+		/* Find size of log and print to std output */
+		clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG,
+			0, NULL, &log_size);
+		program_log = (char*)malloc(log_size + 1);
+		program_log[log_size] = '\0';
+		clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG,
+			log_size + 1, program_log, NULL);
+		printf("%s\n", program_log);
+		free(program_log);
+		getchar();
+		exit(1);
+	}
 
-   return program;
+	return program;
 }
 
 int main(int argc, char **argv) {
@@ -208,7 +208,7 @@ int main(int argc, char **argv) {
 
 	err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &input_image);
 	err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &output_image2);
-	
+
 
 
 
@@ -247,26 +247,26 @@ int main(int argc, char **argv) {
 		exit(1);
 	}
 
-	   /* Perform successive stages of the reduction */
-   while (glob_size / loc_size > loc_size) {
-	   glob_size = glob_size / loc_size;
-	   err = clEnqueueNDRangeKernel(queue, vector_kernel, 1, NULL, &glob_size,
-		   &loc_size, 0, NULL, NULL);
-	   if (err < 0) {
-		   perror("Couldn't enqueue the kernel");
-		   exit(1);
-	   }
+	/* Perform successive stages of the reduction */
+	while (glob_size / loc_size > loc_size) {
+		glob_size = glob_size / loc_size;
+		err = clEnqueueNDRangeKernel(queue, vector_kernel, 1, NULL, &glob_size,
+			&loc_size, 0, NULL, NULL);
+		if (err < 0) {
+			perror("Couldn't enqueue the kernel");
+			exit(1);
+		}
 	}
 	glob_size = glob_size / loc_size;
 	err = clEnqueueNDRangeKernel(queue, complete_kernel, 1, NULL, &glob_size,
-	   NULL, 0, NULL, NULL);
+		NULL, 0, NULL, NULL);
 
 	/* Read the result */
 	err = clEnqueueReadBuffer(queue, sum_buffer, CL_TRUE, 0,
-	   sizeof(float), &sum, 0, NULL, NULL);
+		sizeof(float), &sum, 0, NULL, NULL);
 	if (err < 0) {
-	   perror("Couldn't read the buffer");
-	   exit(1);
+		perror("Couldn't read the buffer");
+		exit(1);
 	}
 
 
@@ -319,8 +319,8 @@ int main(int argc, char **argv) {
 
 
 
-   /* Wait for key press before exiting */
-   std::cout << "Average luminance (found using parrellel reduction): " << sum/(h*w) << std::endl;
+	/* Wait for key press before exiting */
+	std::cout << "Average luminance (found using parrellel reduction): " << sum / (h*w) << std::endl;
 
 	/* Read the image object */
 	origin[0] = 0; origin[1] = 0; origin[2] = 0;
@@ -359,24 +359,24 @@ int main(int argc, char **argv) {
 
 	/* Create output BMP file and write data */
 	storeRGBImage(outputImage, OUTPUT_FILE, h, w, INPUT_FILE);
-   /* Create output BMP file and write data */
+	/* Create output BMP file and write data */
 
-   getchar();
+	getchar();
 
-   /* Deallocate resources */
-   free(inputImage);
-   free(outputImage);
-   clReleaseMemObject(sum_buffer);
-   clReleaseMemObject(data_buffer);
-   clReleaseKernel(vector_kernel);
-   clReleaseKernel(complete_kernel);
-   clReleaseMemObject(input_image);
-   clReleaseMemObject(output_image);
-   clReleaseMemObject(output_input);
-   clReleaseKernel(kernel4a);
-   clReleaseKernel(kernel4b);
-   clReleaseCommandQueue(queue);
-   clReleaseProgram(program);
-   clReleaseContext(context);
-   return 0;
+	/* Deallocate resources */
+	free(inputImage);
+	free(outputImage);
+	clReleaseMemObject(sum_buffer);
+	clReleaseMemObject(data_buffer);
+	clReleaseKernel(vector_kernel);
+	clReleaseKernel(complete_kernel);
+	clReleaseMemObject(input_image);
+	clReleaseMemObject(output_image);
+	clReleaseMemObject(output_input);
+	clReleaseKernel(kernel4a);
+	clReleaseKernel(kernel4b);
+	clReleaseCommandQueue(queue);
+	clReleaseProgram(program);
+	clReleaseContext(context);
+	return 0;
 }
